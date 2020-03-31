7cf20ae243efb8d3af907e834825477ff813de77
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-03-31 18:50:43 -0500

    Fixed bug in case command

--- commands/case.js
+++ commands/case.js
@@ -3,8 +3,8 @@ module.exports = {
   description: 'converts text to random case',
   execute(message, args) {
     let transformed = '';
-    for (let i = 0; i < message.substring(6).length; i++) {
-      let char = message.substring(6).charAt(i);
+    for (let i = 0; i < message.content.substring(6).length; i++) {
+      let char = message.content.substring(6).charAt(i);
       transformed +=
         Math.random() > 0.5 ? char.toUpperCase() : char.toLowerCase();
     }
