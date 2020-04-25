c6bb398608980b0b6eef08f60d4da96f51f601a0
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-04-25 14:41:20 -0500

    Added more images in arjun command

--- commands/arjun.js
+++ commands/arjun.js
@@ -2,9 +2,19 @@ module.exports = {
   name: 'arjun',
   description: 'Sends a spicy picture of arjun being horny af',
   execute(message, args) {
-    message.channel.send({
-      files: ['./images/arjun.png']
-    });
+    if (isNaN(Object.keys(args)[0]))
+      return message.reply(
+        `You need to enter a valid parameter for ${process.env.prefix}arjun <int>`
+      );
+    let num = Object.keys(args)[0];
+    if (num === '1')
+      message.channel.send({
+        files: ['./images/arjun.png'],
+      });
+    else if (num === '2')
+      message.channel.send({
+        files: ['./images/dumbass.png'],
+      });
     message.delete();
-  }
+  },
 };
