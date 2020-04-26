b7ec3614cfc02c8b94b83bbb9185ac0d9538040c
Mahesh Natamai <maheshkumar.natamai@gmail.com>
2020-04-26 14:39:47 -0500

    More minor refactoring

--- cuhlacioBot.js
+++ cuhlacioBot.js
@@ -9,34 +9,33 @@ client.mongoose = require('./utils/mongoose');
 client.config = require('./config.js');
 client.loader = require('./modules/Loader');
 
-process.env.TOKEN =
-  // Consider moving cron.job to a module file or functions.js file. The bot.js / index.js should be a small as possible.
-  cron
-    .job(
-      client.config.clearSchedule,
-      () => {
-        console.log('executing');
-        let server = client.guilds.cache.get(client.config.guildID);
-        let channels = server.channels.cache;
-        channels.forEach((channel, key, map) => {
-          if (channel instanceof Discord.TextChannel) {
-            if (
-              channel.name === 'join-log' ||
-              channel.name === 'announcements' ||
-              channel.name === 'computer-science' ||
-              channel.name === 'memes'
-            )
-              return;
-            client.commands.get('clear').clear(channel);
-            console.log(`Cleared channel '${channel.name}'`);
-          }
-        });
-      },
-      undefined,
-      true,
-      'America/Chicago'
-    )
-    .start();
+// Consider moving cron.job to a module file or functions.js file. The bot.js / index.js should be a small as possible.
+cron
+  .job(
+    client.config.clearSchedule,
+    () => {
+      console.log('executing');
+      let server = client.guilds.cache.get(client.config.guildID);
+      let channels = server.channels.cache;
+      channels.forEach((channel, key, map) => {
+        if (channel instanceof Discord.TextChannel) {
+          if (
+            channel.name === 'join-log' ||
+            channel.name === 'announcements' ||
+            channel.name === 'computer-science' ||
+            channel.name === 'memes'
+          )
+            return;
+          client.commands.get('clear').clear(channel);
+          console.log(`Cleared channel '${channel.name}'`);
+        }
+      });
+    },
+    undefined,
+    true,
+    'America/Chicago'
+  )
+  .start();
 
 const init = async () => {
   console.clear();
@@ -45,11 +44,7 @@ const init = async () => {
   await loader.registerCommands(client);
   await loader.registerEvents(client);
   await loader.checkDiscordStatus(client);
-  try {
-    await client.mongoose.init();
-  } catch (err) {
-    await client.logger.warn('URI needs to be defined for mongoose.');
-  }
+  await client.mongoose.init();
   await client.login(process.env.TOKEN);
 };
 
