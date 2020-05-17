2612a89de2c0cfcca654ee47aabf4794699b5c09
tawfiqsiam <toto.siam999@gmail.com>
2020-05-18 00:27:54 +0300

    Update spank.js

--- commands/spank.js
+++ commands/spank.js
@@ -8,7 +8,7 @@ module.exports = {
   execute(message, args) {
     let searchTerm = `amine spank gif` ;
     var options = {
-      url: 'https://api.tenor.com/v1/search?q=' + searchTerm ,
+      url: 'https://api.tenor.com/v1/search?q=' + searchTerm `&key=LIVDSRZULELA&limit=8`,
       method: 'GET',
       headers: {
         Accept: 'text/html',
